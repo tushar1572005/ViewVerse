1eeb63363d81ecabe6455d2aa4bcd0832c684294
Tushar1572005 <tusharchaudhari1572005@gmail.com>
2026-03-28 12:26:52 +0530

    access token refresh token and auth middleware
    Co-authored-by: suryawanshibhavesh55-del <suryawanshibhavesh55-del@users.noreply.github.com>

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -61,7 +61,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccesToken = function(){
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
             _id: this._id,
@@ -76,7 +76,7 @@ userSchema.methods.generateAccesToken = function(){
     )
 }
 
-userSchema.methods.generateAccesToken = function(){
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
             _id: this._id,

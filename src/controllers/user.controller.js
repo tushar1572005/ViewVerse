7cd217f14c0e928ad766baacac62487dd0ed685c
Tushar1572005 <tusharchaudhari1572005@gmail.com>
2026-04-01 11:32:30 +0530

    fix: remove refreshToken field on user logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -151,7 +151,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
 const logoutUser = asyncHandler(async (req, res) => {
     User.findByIdAndUpdate(req.user._id, {
-        $set: { refreshToken: undefined }
+        $unset: { refreshToken: 1 // this removes the field from document 
+        }
     },
         {
             new: true
